50b1cde14fb575fadc1b661bf8df7a1e5dd98b87
slm123 <slm123@qq.com>
2021-03-13 20:04:31 +0800

    完成了后台主页渲染用户头像和名称

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,10 @@ $.ajaxPrefilter(function (options) {
     console.log(options);
     // 手动添加前缀 拼接对应环境开发的服务器地址
     options.url = baseURL + options.url
+    // 身份验证
+    if (options.url.indexOf("/my/") !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -5,11 +5,12 @@ $(function () {
 // 后面其他的页面还要调用
 function getUserInfo() {
     $.ajax({
+        type: 'GET',
         url: '/my/userinfo',
-        headers: {
-            // 重新登录，因为token过期事件 12小时
-            Authorization: localStorage.getItem("token") || ""
-        },
+        // headers: {
+        //     // 重新登录，因为token过期事件 12小时
+        //     Authorization: localStorage.getItem("token") || ""
+        // },
         success: (res) => {
             // console.log(res);
             if (res.status != 0) {
@@ -24,7 +25,7 @@ function renderAvatar(user) {
     var name = user.nickname || user.username
     $("#welcome").html("欢迎&nbsp;&nbsp;" + name)
     if (user.user_pic !== null) {
-        $(".layui-nav-img").show().attr("src", use.user_pic)
+        $(".layui-nav-img").show().attr("src", use.user_pic).show()
         $(".tetx-avatar").hide()
     } else {
         $(".layui-nav-img").hide()
